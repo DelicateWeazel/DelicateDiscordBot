47bcf37238104b415ec173e522ebc70da4486b44
DelicateWeazel <yew@noslickmx.xyz>
2020-07-30 05:44:35 +1200

    index.js edited

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 //Start
 const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+//-------------------Debugy thing--------------------\\
+const testing = true;
+function testing(message) {
+    if (testing) {
+        console.log("TESTING: " + message);
+    }
+}
+//-------------------Debugy thing--------------------\\

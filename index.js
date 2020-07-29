66610f5d8fa3eeee412abf814df751c824c7b3b4
DelicateWeazel <yew@noslickmx.xyz>
2020-07-30 06:56:45 +1200

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ function test(message) {
 //-------------and it does display-------------\\
 //test("this should display");
 //-------------and it does display-------------\\
+
+
+//-------------Discord Bot-------------\\
+
+bot.on("ready", () => {
+    generalChannel = bot.channels.cache.get('731959489036812310')
+    test(`${bot.user.tag} is now online`)
+   
+    generalChannel.send("Hello, World! I am online :)")
+});
+
+bot.on("message", message => {
+    //make a embed and send it to #info
+    
+    
+    if (message.content === '-test') {
+        if (message.author.bot) return; //prevents the bot from replying to itself
+    infoChannel = bot.channels.cache.get('738107502255669336')
+    
+    infoChannel.send(info)
+  }
+});
+
+bot.on("guildMemberAdd" , () => {
+    //nothing
+});
+
+bot.login(nothing to see here);
+//-------------Discord Bot-------------\\
